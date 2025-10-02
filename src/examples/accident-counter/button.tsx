6b38587161cdc12591d35f36d01ea5f8334d324f
Steve Kinney <hello@stevekinney.net>
2025-10-02 12:05:48 -0500

    Button component mirror <button />

--- src/examples/accident-counter/button.tsx
+++ src/examples/accident-counter/button.tsx
@@ -1,7 +1,10 @@
-export const Button = ({ children }: { children: string }) => {
+import type { ComponentPropsWithoutRef } from 'react';
+
+export const Button = (props: ComponentPropsWithoutRef<'button'>) => {
   return (
-    <button className="bg-primary-400 hover:bg-primary-500 rounded px-4 py-2 font-bold text-white">
-      {children}
-    </button>
+    <button
+      className="bg-primary-400 hover:bg-primary-500 rounded px-4 py-2 font-bold text-white"
+      {...props}
+    />
   );
 };
